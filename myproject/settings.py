bf0feeed1f8fafcc9be3c686ad8647ff0e0b889e
Mithanshu Hedau <144697427+MithanshuHedau@users.noreply.github.com>
2025-07-02 16:47:14 +0530

    allowed_host

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-x$&&a!f#$$$i!1+szp8g-9ylt637clw6dviks8$ru$@9386co7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zingkesblog.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
